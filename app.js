5f3c149784d3884d1ac0fe7941bee04f712b993f
Harshvardhan Dubey <20egjcs041@gitjaipur.com>
2022-07-31 00:44:16 +0530

    commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function createBubble(){
+    const section = document.querySelector('section')
+    const createElement = document.createElement('span')
+    var size = Math.random() * 69;
+    createElement.style.width = 20 + size + 'px';
+    createElement.style.height =  20 + size + 'px';
+    createElement.style.left =  Math.random() * innerWidth + 'px';
+    section.appendChild(createElement);
+    setTimeout(() => {
+        createElement.remove()
+    },4000)
+}
+setInterval(createBubble,50)
